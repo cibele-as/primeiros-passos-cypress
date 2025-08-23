e2b327ac74d85eadd324c457b35b640451816af9
cibele <cibele.as10@gmail.com>
2025-08-23 16:50:52 +0200

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -4,21 +4,29 @@ beforeEach(() => {
   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 })
 
+const selectorsList = {
+  usernameField: 'input[name="username"]',
+  passwordField: 'input[name="password"]',
+  loginButton: 'button[type="submit"]',
+  sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+  wrongCredentialsAlert: "[role='alert']"
+}
+
 // Check login succes wiith valid username and password
   it('Login Sucess', () => {
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').should('contain', 'Dashboard')
+    cy.get(selectorsList.sectionTitleTopBar).should('contain', 'Dashboard')
   })
 
 // Check login failed with invalid username and password
   it('Login Failed', () => {  
-    cy.get('input[name="username"]').type('Test')
-    cy.get('input[name="password"]').type('test123')
-    cy.get('button[type="submit"]').click()
-    cy.get("[role='alert']")
+    cy.get(selectorsList.usernameField).type('Test')
+    cy.get(selectorsList.passwordField).type('test123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialsAlert)
   })
   
 })
