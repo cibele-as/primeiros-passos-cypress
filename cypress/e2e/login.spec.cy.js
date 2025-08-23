2b15031406773bf08a54f815f54f2453bcadf0d4
cibele <cibele.as10@gmail.com>
2025-08-23 17:29:16 +0200

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Test', () => {
 
 beforeEach(() => {
@@ -9,22 +11,23 @@ const selectorsList = {
   passwordField: 'input[name="password"]',
   loginButton: 'button[type="submit"]',
   sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+  dashboardGrid: '.orangehrm-dashboard-grid',
   wrongCredentialsAlert: "[role='alert']"
 }
 
 // Check login succes wiith valid username and password
-  it('Login Sucess', () => {
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+  it('Login Success', () => {
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).should('contain', 'Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
 
 // Check login failed with invalid username and password
   it('Login Failed', () => {  
-    cy.get(selectorsList.usernameField).type('Test')
-    cy.get(selectorsList.passwordField).type('test123')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialsAlert)
   })
